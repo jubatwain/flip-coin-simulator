19adb32a10a4c0c8a709d215633e4a9894454b41
Mark Neptune <102503462+jubatwain@users.noreply.github.com>
2023-06-24 10:03:09 +0300

    Create coin_flip.py

---
+++ coin_flip.py
@@ -0,0 +1,9 @@
+import random
+
+random_number =random.randint(0,1)
+if random_number == 1:
+    random_number ="Heads"
+elif random_number == 0:
+    random_number ="Tails"
+
+print (random_number)
